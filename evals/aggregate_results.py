504f24f9749f738c681e41f008dab988ba6ad08a
alanzty <tianyuzhu52@gmail.com>
2024-10-08 17:00:50 +1100

    added hf datasets and model eval.

--- evals/aggregate_results.py
+++ evals/aggregate_results.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import os
 import argparse
+import json
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--exp-path", default=None)
-    parser.add_argument("--metrics", default="['mAP@1000', 'mrr@1000', 'NDCG@10', 'mERR', 'mRBP9']")
-    parser.add_argument("--evals", default=None)
+    parser.add_argument("--exp-path", default="/MarqoModels/GE/marqo-ecommerce-B")
+    parser.add_argument("--metrics", default="['MAP@1000', 'P@10', 'Recall@10', 'MRR@1000', 'NDCG@10']")
+    parser.add_argument("--evals", default="['gs-title2image', 'gs-query2image', 'ap-name2image']")
 
     args = parser.parse_args()
 
@@ -15,16 +16,19 @@ if __name__ == "__main__":
 
     metrics = eval(args.metrics)
     exp_path = args.exp_path
-    dirs = os.listdir(exp_path)
+    exp_dirs = [os.path.join(exp_path, n) for n in evals]
 
-    all_results = []
-    for dir in dirs:
-        if dir in evals:
-            df = pd.read_csv(os.path.join(exp_path, dir, "output.csv"))
-            df["name"] = [dir]
-            all_results.append(df)
-    all_results = pd.concat(all_results)
-    all_results = all_results[["name"] + metrics]
-    all_results = all_results.sort_values("name")
+    all_results = {"name": []}
+    all_results.update({m: [] for m in metrics})
+    for exp_path in exp_dirs:
+        with open(os.path.join(exp_path, "output.json"), "r") as f:
+            results = json.load(f)
+        del results['summary']
+        results = {k: v for d in results.values() for k, v in d.items() if k in all_results}
+        results["name"] = os.path.basename(exp_path)
+        for k in all_results:
+            all_results[k].append(results[k])
+
+    all_results = pd.DataFrame(all_results)
     all_results.to_csv(os.path.join(exp_path, "results_sum.csv"), index=False)
     os.system(f"cat {os.path.join(exp_path, 'results_sum.csv')}")
--- evals/eval_dataset_loader.py
+++ evals/eval_dataset_loader.py
@@ -2,6 +2,7 @@ from torch.utils.data import Dataset
 from PIL import Image
 import numpy as np
 import pandas as pd
+import io
 
 
 class MMEvalDataset(Dataset):
@@ -42,6 +43,7 @@ class MFRightEvalDataset(Dataset):
         self.tokenize = tokenizer
         self.context_length = args.context_length
         self.side = side
+        self.is_hf_dataset = hasattr(args, 'hf_dataset') and args.hf_dataset
 
     def __len__(self):
         return len(self.right_inputs)
@@ -52,7 +54,8 @@ class MFRightEvalDataset(Dataset):
             if cat == "txt":
                 rights.append(self.tokenize([str(self.right_inputs[idx][j])], context_length=self.context_length)[0])
             else:
-                rights.append(self.transforms(Image.open(str(self.right_inputs[idx][j]))))
+                img = str(self.right_inputs[idx][j]) if not self.is_hf_dataset else io.BytesIO(self.right_inputs[idx][j]['bytes'])
+                rights.append(self.transforms(Image.open(img)))
 
         right_weight = np.array(self.right_weights)
 
@@ -62,6 +65,87 @@ class MFRightEvalDataset(Dataset):
 
         right_weight = right_weight / sum(right_weight)
         return rights, right_weight
+
+
+# class MFRightEvalDatasetRaw(Dataset):
+#     def __init__(self, doc_meta_list, args, side=0):
+#         self.img_or_txt = args.img_or_txt
+#
+#         if side == 0:
+#             side_keys = args.left_keys
+#             side_weights = args.left_weights
+#         else:
+#             side_keys = args.right_keys
+#             side_weights = args.right_weights
+#
+#         self.right_inputs = [[doc[key] for key in side_keys] for doc in doc_meta_list]
+#
+#         self.right_weights = side_weights
+#         self.context_length = args.context_length
+#         self.side = side
+#         self.is_hf_dataset = args.hf_dataset is not None
+#
+#     def __len__(self):
+#         return len(self.right_inputs)
+#
+#     def __getitem__(self, idx):
+#         rights = []
+#         for j, cat in enumerate(self.img_or_txt[self.side]):
+#             if cat == "txt":
+#                 rights.append(str(self.right_inputs[idx][j]))
+#             else:
+#                 img = str(self.right_inputs[idx][j]) if not self.is_hf_dataset else io.BytesIO(self.right_inputs[idx][j]['bytes'])
+#                 rights.append(img)
+#
+#         right_weight = np.array(self.right_weights)
+#
+#         for i in range(1, len(right_weight)):
+#             if not pd.notna(self.right_inputs[idx][i]):
+#                 right_weight[i] = 0
+#
+#         right_weight = right_weight / sum(right_weight)
+#         return rights, right_weight
+#
+#
+# class MFRightEvalDatasetSingleProc(Dataset):
+#     def __init__(self, doc_meta_list, args, preprocess, side=0):
+#         self.img_or_txt = args.img_or_txt
+#
+#         if side == 0:
+#             side_keys = args.left_keys
+#             side_weights = args.left_weights
+#         else:
+#             side_keys = args.right_keys
+#             side_weights = args.right_weights
+#
+#         self.right_inputs = [[doc[key] for key in side_keys] for doc in doc_meta_list]
+#
+#         self.right_weights = side_weights
+#         self.context_length = args.context_length
+#         self.side = side
+#         self.is_hf_dataset = args.hf_dataset is not None
+#         self.preprocess = preprocess
+#
+#     def __len__(self):
+#         return len(self.right_inputs)
+#
+#     def __getitem__(self, idx):
+#         rights = []
+#         for j, cat in enumerate(self.img_or_txt[self.side]):
+#             if cat == "txt":
+#                 rights.append(self.preprocess(text=str(self.right_inputs[idx][j]), padding='max_length', return_tensors="pt")['input_ids'][0])
+#             else:
+#                 img = self.preprocess(images=[Image.open(io.BytesIO(self.right_inputs[idx][j]['bytes'])).convert('RGB')], padding='max_length', return_tensors="pt")['pixel_values'][0]
+#                 rights.append(img)
+#
+#         right_weight = np.array(self.right_weights)
+#
+#         for i in range(1, len(right_weight)):
+#             if not pd.notna(self.right_inputs[idx][i]):
+#                 right_weight[i] = 0
+#
+#         right_weight = right_weight / sum(right_weight)
+#         return rights, right_weight
 
 
 class MFRightEvalDatasetTest(Dataset):
